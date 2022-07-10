a4f000c2aebad4876a9fc699ab007069e585981f
Cauvery Raja S <cauveryraja25@gmail.com>
2022-07-10 13:42:14 +0530

    Update Dev image and remove canvas

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-import { DEVELOPER, CANVAS } from "./constants/images.js";
+import { DEVELOPER, CANVAS, DEV_UNDER_TREE, SUN_WITH_CLOUDS } from "./constants/images.js";
 import view from './src/view.js';
 import model from './src/model.js';
 
-view.loadImages(DEVELOPER, CANVAS);
+view.loadImages(DEV_UNDER_TREE, SUN_WITH_CLOUDS);
 
 (async function renderStringsInLoop(strings) {
     await view.renderStrings(strings);
     renderStringsInLoop(strings);
 })(model.roles);
+
+let plant = document.getElementById('plant-2');
+let branch = document.getElementById('Vector_20');
+
+plant.classList.add('move');
+branch.classList.add('move');
